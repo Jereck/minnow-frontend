e662f6ee1ccba56db030d8367ad28aabddda73a4
Jake Reck <jake@jakes-mac-mini.lan>
2021-09-28 09:36:01 -0400

    Fixing graphql connection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache()
 })
 
