b80d475e783612da3ec330c2ed0ab079b3dba7b6
Jake Reck <jake@jakes-mac-mini.lan>
2021-10-06 13:24:28 -0400

    Fixing a few things

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -1,27 +1,8 @@
 import React from 'react'
-import { useQuery, gql } from '@apollo/client'
 
 import './home.styles.scss'
 
-const USERS = gql`
-  query {
-    findUsers {
-      id
-      firstName
-      lastName
-      userName
-    }
-  }
-`
-
 function Home() {  
-  const { loading, error } = useQuery(USERS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) {
-    console.log(error);
-    return <p>Error :(</p>;
-  }
 
   return (
     <>
@@ -30,10 +11,6 @@ function Home() {
       </div>
       <h1 style={{ textAlign: 'center', fontWeight: 100, fontSize: '3em' }}>Readers</h1>
       <hr style={{ width: '80%' }} />
-
-      <div>
-        
-      </div>
     </>
   )
 }
